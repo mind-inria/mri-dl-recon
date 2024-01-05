d814a65d6255a8881004379511940845b6c282c9
Lena OUDJMAN <lo276838@is231077.intra.cea.fr>
2024-01-05 16:34:26 +0100

    suite, def'

--- src/mri_dlrecon/functions_torch/utils_torch.py
+++ src/mri_dlrecon/functions_torch/utils_torch.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import torch
+import h5py
 
 def show_coils(data, slice_nums, cmap=None):
     fig = plt.figure()
@@ -66,3 +67,19 @@ def virtual_coil_reconstruction(imgs):
     )
 
     return img_comb
+
+
+def load_and_transform(path):
+    hf = h5py.File(path)
+    kspace = hf['kspace'][()]
+    kspace = torch.tensor(kspace , dtype=torch.complex64)
+    return kspace
+
+def create_masked_Image(mask, kspace,):
+    masked_data , _ = mask(kspace.shape) 
+    masked_kspace = kspace * masked_data
+    masked_image = torch.fft.fftshift(torch.fft.ifft2(masked_kspace))
+
+    masked_image = masked_image.unsqueeze(1)
+    masked_image = masked_image.abs()
+    return masked_image
