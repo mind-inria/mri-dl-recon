00a22e44e704388a13e3a6b8cd70bddf7b903db3
Lena OUDJMAN <lo276838@is231077.intra.cea.fr>
2024-01-12 16:31:13 +0100

    debut xpdnet

---
+++ src/mri_dlrecon/models/functions_torch/xpdnet.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/home/lo276838/Modèles/mri-dl-recon/src')
+
+from denoisers.didn import DIDN
+from mri_dlrecon.models.utils.fourier  import FFT, IFFT
+
---
+++ src/mri_dlrecon/models/utils/masking.py
@@ -0,0 +1,7 @@
+import torch 
+
+def _mask(x):
+    k_data, mask = x
+    mask = mask.unsqueeze(-1).type(k_data.dtype)
+    masked_k_data = torch.mul(mask, k_data)
+    return masked_k_data
